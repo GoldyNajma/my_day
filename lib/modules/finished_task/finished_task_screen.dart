717fc670b9b13cbe10239e0167571f52f4e6256c
GoldyNajma <najma.goldy354@gmail.com>
2021-06-15 13:05:17 +0700

    Implements get all finished tasks on finished task screen

--- lib/modules/finished_task/finished_task_screen.dart
+++ lib/modules/finished_task/finished_task_screen.dart
@@ -2,14 +2,22 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:my_day/data/models/task.dart';
+import 'package:my_day/modules/task/task_view_model.dart';
+import 'package:my_day/utils/functions.dart';
 import 'package:my_day/utils/widgets/my_day_back_app_bar.dart';
+import 'package:my_day/utils/widgets/my_day_rounded_button.dart';
 import 'package:my_day/utils/widgets/my_day_task_item.dart';
 
 import 'widgets/no_finished_task_widget.dart';
 
-class FinishedTaskScreen extends StatelessWidget {
+class FinishedTaskScreen extends StatefulWidget {
   const FinishedTaskScreen({ Key? key }) : super(key: key);
 
+  @override
+  _FinishedTaskScreenState createState() => _FinishedTaskScreenState();
+}
+
+class _FinishedTaskScreenState extends State<FinishedTaskScreen> {
   List<Task> _getSampleTasks() {
     final Random random = Random();
 
@@ -26,32 +34,55 @@ class FinishedTaskScreen extends StatelessWidget {
     .toList();
 
   @override
-  Widget build(BuildContext context) {
-    List<Task> tasks = _getSampleTasks();
-    List<Task> finishedTasks = _getFinishedTasks(tasks); 
-    print('tasks.length: ${tasks.length}');
-    print('finishedTasks.length: ${finishedTasks.length}');
+  Widget build(BuildContext context) => Scaffold(
+    appBar: const MyDayBackAppBar(),
+    body: FutureBuilder<List<Task>>(
+      future: TaskViewModel.instance.getAllTasksAsList(),
+      builder: (futureBuilderContext, snapshot) {
+        if (snapshot.hasData) {
+          List<Task> tasks = snapshot.data!;
+          List<Task> finishedTasks = _getFinishedTasks(tasks); 
 
-    return Scaffold(
-      appBar: const MyDayBackAppBar(),
-      body: finishedTasks.isEmpty 
-        ? MediaQuery.of(context).orientation == Orientation.portrait
-          ? const Center(child: const NoFinishedTaskWidget())
-          : const SingleChildScrollView(
-              child: const Center(
-                child: const Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-                  child: const NoFinishedTaskWidget(),
-                ),
-              ),
+          return finishedTasks.isEmpty 
+            ? MediaQuery.of(context).orientation == Orientation.portrait
+              ? const Center(child: const NoFinishedTaskWidget())
+              : const SingleChildScrollView(
+                  child: const Center(
+                    child: const Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                      child: const NoFinishedTaskWidget(),
+                    ),
+                  ),
+                )
+            : ListView(
+                children: finishedTasks.map((Task task) => MyDayTaskItem(
+                  taskId: task.id,
+                  title: task.title,
+                  checked: task.checked,
+                )).toList()
+              );
+        } else if (snapshot.hasError) {
+          WidgetsBinding.instance!.addPostFrameCallback((_) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            showTextOnlySnackBar(
+              context: futureBuilderContext, 
+              text: 'Failed to load finished tasks.\n${snapshot.error}'
+            );
+          });
+          
+          return Center(
+            child: MyDayRoundedButton(
+              onPressed: () => setState(() {}),
+              buttonColor: Theme.of(context).accentColor,
+              text: 'Retry load finished tasks',
             )
-        : ListView(
-            children: finishedTasks.map((Task task) => MyDayTaskItem(
-              taskId: task.id,
-              title: task.title,
-              checked: task.checked,
-            )).toList()
-          ),
-    );
-  }
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      }
+    )
+  );
 }
