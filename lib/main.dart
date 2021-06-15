e433a2e9534995f10cbbc82ffd4b27d4a3e8d176
Nawa <dragneel77@gmail.com>
2021-06-15 14:06:27 +0700

    pop up notification

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:my_day/common/theme.dart';
 import 'package:my_day/modules/splash_screen.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
@@ -19,6 +20,28 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    // showSimpleNotification(
+    //     Text("Notifikasi Masuk"),
+    //     position: NotificationPosition.top,
+    //     subtitle: Text(message.notification!.body!),
+
+    // background: Colors.green);
+    showOverlayNotification((context) {
+      return SafeArea(
+        child: Card(
+            elevation: 4,
+            color: Colors.blue[50],
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            child: ListTile(
+              leading: Icon(Icons.message),
+              title: Text(message.notification!.title!),
+              // subtitle: Text(message.notification!.body!),
+            )),
+      );
+    });
+  });
+
   FirebaseMessaging.instance.getToken().then((value) => print(value));
   runApp(MyApp());
 }
@@ -26,10 +49,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return OverlaySupport.global(
+        child: MaterialApp(
       title: 'My Day',
       theme: getThemeData(),
       home: const SplashScreen(),
-    );
+    ));
   }
 }
